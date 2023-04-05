77840dcc04b16543eff204e5243970b07742f4fd
Subroto Biswas <subroto.iu@gmail.com>
2023-04-05 12:29:16 +0600

    Update Footer.js

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer>
-      <p className='bg-dark text-light text-center'>Copyright &copy; Subroto April aa 2023.</p>
+      <p className='bg-dark text-light text-center'>Copyright &copy; Subroto April - 2023</p>
     </footer>
   )
 }
