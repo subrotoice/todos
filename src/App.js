c5064f2d473725b4657a8d317db82e39fab4d76c
subrotoice <subroto.iu@gmail.com>
2023-12-08 18:49:55 +0600

    Filter without return and item add first

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
-import './App.css'; // Include CSS External
+import "./App.css"; // Include CSS External
 import Header from "./MyComponents/Header";
 import Todos from "./MyComponents/Todos";
 import Footer from "./MyComponents/Footer";
-import {AddTodo} from "./MyComponents/AddTodo";
-import { useEffect, useState } from 'react';
+import { AddTodo } from "./MyComponents/AddTodo";
+import { useEffect, useState } from "react";
 
 function App() {
   let initTodos;
-  if(localStorage.getItem('todos')===null) { // checking local storage and initTodos Seted to state variable
-    initTodos = [];   
+  if (localStorage.getItem("todos") === null) {
+    // checking local storage and initTodos Seted to state variable
+    initTodos = [];
   } else {
-    initTodos = JSON.parse(localStorage.getItem('todos'));
+    initTodos = JSON.parse(localStorage.getItem("todos"));
   }
-  const onDelete = (todo)=> {
+  const onDelete = (todo) => {
     console.log("I am onDelete", todo);
-    setTodos(todos.filter((e)=>{ // useEffect will update memory.
-      return e!==todo;
-    }))
-  }
+    setTodos(todos.filter((e) => e !== todo));
+  };
 
-  const addTodo = (title, desc)=>{
+  const addTodo = (title, desc) => {
     let sno;
-    if (todos.length==0) {
+    if (todos.length === 0) {
       sno = 0;
     } else {
-      sno = todos[todos.length-1].sno+1;
+      sno = todos[todos.length - 1].sno + 1;
     }
-    
+
     let myTodo = {
       sno: sno,
       title: title,
-      desc: desc
-    }
-    setTodos([...todos, myTodo]); // 2nd item not an array so no ... Spread operator
-  }
+      desc: desc,
+    };
+    setTodos([myTodo, ...todos]);
+  };
   const [todos, setTodos] = useState(initTodos);
-  // Think: when setTodos call then all code in useEffect will execute. 
-  useEffect(()=>{
-    localStorage.setItem('todos', JSON.stringify(todos));
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
-  {/* Think React way: All code is here, Just include from other way */} 
-  return ( 
-    <>  
-      <Header title="Subroto's Todos" searchBar={false} /> {/* bool value er joonno {} */} 
+  {
+    /* Think React way: All code is here, Just include from other way */
+  }
+  return (
+    <>
+      <Header title="Subroto's Todos" searchBar={false} />
       <AddTodo addTodo={addTodo} />
       <Todos todos={todos} onDelete={onDelete} />
       <Footer />
--- src/MyComponents/AddTodo.js
+++ src/MyComponents/AddTodo.js
@@ -1,36 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-export const AddTodo = (props) => {  // Taking function as argument(props)
+export const AddTodo = (props) => {
+  // Taking function as argument(props)
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
-  const submit = (e)=>{  // OnSubmit has this function
+  const submit = (e) => {
+    // OnSubmit has this function
     e.preventDefault();
-    if(!title || !desc) {
+    if (!title || !desc) {
       alert("Title and Description can not be Blank");
     } else {
       props.addTodo(title, desc);
       setTitle("");
       setDesc("");
     }
-  }
+  };
 
   return (
-    <div className='container py-3'>
-        {/* {localStorage.setItem("lastname", "Smith")}
+    <div className="container py-3">
+      {/* {localStorage.setItem("lastname", "Smith")}
         {localStorage.getItem("lastname")}
         {window.localStorage.setItem('user', JSON.stringify(person))} // JSON fromat
         {localStorage.getItem('user')} */}
-        <form onSubmit={submit}>
-            <div className="mb-3">
-                <label htmlFor="title" className="form-label">Title</label>
-                <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} className="form-control" id="title" />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="description" className="form-label">Description</label>
-                <input type="text" value={desc} onChange={(e)=>setDesc(e.target.value)} className="form-control" id="description" />
-            </div>
-            <button type="submit" className="btn btn-sm btn-success">Add Todo</button>
-        </form>
+      <form onSubmit={submit}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+            id="title"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Description
+          </label>
+          <input
+            type="text"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+            className="form-control"
+            id="description"
+          />
+        </div>
+        <button type="submit" className="btn btn-sm btn-success">
+          Add Todo
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
